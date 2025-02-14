5fc6dd41f0c451d5be4335c2f015b6d6affcfec0
m.kirdin <anakin03@mail.ru>
2025-02-14 18:46:01 +0300

    Almost did Paragraph 7.4

--- algo/part7/tasks74/task4.cpp
+++ algo/part7/tasks74/task4.cpp
@@ -24,8 +24,6 @@ template<typename T> std::vector<T> subvector(const std::vector<T>& vtr, const s
 
 template<typename T> uint32_t merge(const std::vector<T>& lhs, const std::vector<T>& rhs, std::vector<T>& vtr)
 {
-    print_vector(lhs.begin(), lhs.end());
-    print_vector(rhs.begin(), rhs.end());
     uint32_t inversions(0);
     size_t lidx(0), ridx(0), l(lhs.size()), r(rhs.size());
     std::vector<T> merged;
@@ -59,8 +57,6 @@ template<typename T> uint32_t merge(const std::vector<T>& lhs, const std::vector
         }
     }
     vtr = std::move(merged);
-    print_vector(vtr.begin(), vtr.end());
-    std::cout << "------" << std::endl;
     return inversions;
 }
 
@@ -80,51 +76,6 @@ template<typename T> uint32_t msort(std::vector<T>& vtr, const size_t& n)
     return inversions;
 }
 
-// template<class Iter> uint32_t merge(const Iter& l_begin, const Iter& l_end, const Iter& r_begin, const Iter& r_end)
-// {
-//     uint32_t inversions(0);
-
-//     std::cout << "current step:" << std::endl;
-//     print_vector(l_begin, r_end);
-
-//     for (Iter i = r_begin; i != r_end; i++)
-//     {
-//         for (size_t j = i - l_begin; j >= 1; j--)
-//         {
-//             if (*(l_begin + j) < *(l_begin + j - 1))
-//             {
-//                 inversions++;
-//                 std::iter_swap(l_begin + j, l_begin + j - 1);
-//             }
-//             else
-//             {
-//                 break;
-//             }
-//         }
-//     }
-//     print_vector(l_begin, r_end);
-//     return inversions;
-// }
-
-// template<class Iter> uint32_t msort(const Iter& begin, const Iter& end)
-// {
-//     if (end - begin > 1)
-//     {
-//         uint32_t inversions(0);
-//         size_t pivot = (end - begin) / 2;
-//         std::cout << end - begin << ": " << *begin << " " << *(end-1) << std::endl;
-//         std::cout << pivot + (end - begin) % 2 - 1 << " " << pivot << std::endl;
-//         inversions += msort(begin, begin + pivot + (end - begin) % 2);
-//         inversions += msort(begin + pivot, end);
-//         inversions += merge(begin, begin + pivot + (end - begin) % 2, begin + pivot, end);
-//         return inversions;
-//     }
-//     else
-//     {
-//         return 0;
-//     }
-// }
-
 int main()
 {
     size_t n;
@@ -138,7 +89,6 @@ int main()
         vtr.push_back(input);
     }
     std::cout << msort(vtr, vtr.size()) << std::endl;
-    print_vector(vtr.begin(), vtr.end());
 
     return 0;
 }
